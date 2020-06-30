e4c7026c177f0f43b54e4c3966f857ee41c063a4
shreya0167 <shreya.maheshwari06@gmail.com>
2020-06-30 13:34:16 +0530

    Kth symbol in grammar using recursion

---
+++ Recursion/reverseStack.java
@@ -0,0 +1,13 @@
+//Do it without using another stack
+import java.util.*;
+
+class Solution {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        stack.push(5);
+    }
+}
