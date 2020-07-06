296cbc27382114cfbba00b6f4cd975bd717567d2
shreya0167 <shreya.maheshwari06@gmail.com>
2020-07-07 01:05:42 +0530

    Permutation with case change recursion problem

--- Recursion/permutationWithCaseChange.java
+++ Recursion/permutationWithCaseChange.java
@@ -17,10 +17,11 @@ class Solution {
         }
         String op1 = op;
         String op2 = op;
-        //Adding 0th index character
+        //Adding 0th index character to op1 and op2
         char ch = ip.charAt(0);
-        op2 = op2 + ch;
-        //Erasing first index of input
+        op1 = op1 + ch;
+        op2 = op2 + Character.toUpperCase(ch);
+        //Erasing first index of input to create SMALLER INPUT
         ip = ip.substring(1, ip.length());
         
         solve(ip, op1);
@@ -30,5 +31,5 @@ class Solution {
     }
 }
 
-// IP : abc
-// OP : c b bc a ac ab abc
+// IP : ab
+// OP : ab aB Ab AB
