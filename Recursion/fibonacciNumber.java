fbb815395656f2b12590a6c648a3c537ded2c6fc
shreya0167 <shreya.maheshwari06@gmail.com>
2020-07-15 18:47:51 +0530

    Fibonacci Number using Recursion

---
+++ Recursion/fibonacciNumber.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(fib(n));
+    }
+	public static int fib(int n){
+		if(n == 0 || n == 1){
+            return n;
+        }
+        return fib(n-1)+fib(n-2);
+		
+	}
+
+}
