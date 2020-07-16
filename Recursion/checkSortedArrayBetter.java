8c43f47336002443c11d4c399fe3b0d3f5ed0c07
shreya0167 <shreya.maheshwari06@gmail.com>
2020-07-16 23:34:49 +0530

    Check sorted array problem with the use of recursion and  helper functions

--- Recursion/checkSortedArrayBetter.java
+++ Recursion/checkSortedArrayBetter.java
@@ -1,5 +1,5 @@
 class Solution {
-    public static boolean checkSortedBetter(int input[], int startIndex){
+    private static boolean checkSortedBetter(int input[], int startIndex){
         if(startIndex >= input.length-1){
             return true;
         }
@@ -10,10 +10,16 @@ class Solution {
         return smallAns;
     }
 
+    public static boolean checkSortedBetter(int input[]){
+        return checkSortedBetter(input, 0);
+    }
+
     public static void main(String[] args) {
         int input[] = {1,2,3};
-        System.out.println(checkSortedBetter(input, 0));
+        System.out.println(checkSortedBetter(input));
     }
 }
 
-//Output: True
+//Imp points
+//This is a better approach because we are saving time and space both. We are not creating small array each time in the recursive call.
+//Also, to help user not pass the startIndex when calling a function we write the above code by creating another function.
