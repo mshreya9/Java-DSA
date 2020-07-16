174984455b5f7652d2ee94b5bf1931fce27d7627
shreya0167 <shreya.maheshwari06@gmail.com>
2020-07-16 23:42:35 +0530

    Check palindrome using recursion and helper function

---
+++ Recursion/checkPalindrome.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+
+	public static boolean isStringPalindrome(String input) {
+        return Solution.check(input, 0, input.length() - 1);
+	}
+    
+    public static boolean check(String s, int start, int end){
+        if(start >= end){
+            return true;
+        }
+        
+        if(s.charAt(start) != s.charAt(end)){
+            return false;
+        }
+        
+        return Solution.check(s, start + 1, end - 1);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        System.out.println(isStringPalindrome(input));
+    }
+}
