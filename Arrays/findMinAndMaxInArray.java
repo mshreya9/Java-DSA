24aae6673e7169ff3ec2dae1ef0b162ad004a69f
shreya0167 <shreya.maheshwari06@gmail.com>
2020-07-21 22:32:14 +0530

    Find min and max in array

--- Arrays/findMinAndMaxInArray.java
+++ Arrays/findMinAndMaxInArray.java
@@ -2,28 +2,26 @@
 
 import java.util.*;
 
-class CheckMinAndMax {
+class MinAndMax {
 
     public static void findMinMax(int[] arr) {
       int minimum = Integer.MAX_VALUE;
-      for(int i = 1; i < arr.length; i++){
+      int maximum = Integer.MIN_VALUE;
+      for(int i = 0; i < arr.length; i++){
           if(arr[i] < minimum){
             minimum = arr[i];
           }
-      } 
-      System.out.println("The minimum number in an array is: " + minimum);
 
-      int maximum = Integer.MIN_VALUE;
-      for(int i = 1; i < arr.length; i++){
           if(arr[i] > maximum){
             maximum = arr[i];
           }
       } 
+      System.out.println("The minimum number in an array is: " + minimum);
       System.out.println("The maximum number in an array is: " + maximum);
     }
 
     public static void main(String[] args){
         int[] arr = {9, 2, 3, 6, 1, 12, 32};
-        CheckMinAndMax.findMinMax(arr);
+        MinAndMax.findMinMax(arr);
     }
   }
