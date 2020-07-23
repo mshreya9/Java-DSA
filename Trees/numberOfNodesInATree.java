2b5dda745e542f1eda0d622cad029e245f8ae82b
shreya0167 <shreya.maheshwari06@gmail.com>
2020-07-24 00:48:54 +0530

    Height of tree

--- Trees/numberOfNodesInATree.java
+++ Trees/numberOfNodesInATree.java
@@ -24,10 +24,10 @@ class TreeUse {
 
     public static void main(String[] args) {
         TreeNode<Integer> root = new TreeNode<Integer>(4);
-        TreeNode<Integer> node1 = new TreeNode<Integer>(4);
-        TreeNode<Integer> node2 = new TreeNode<Integer>(4);
-        TreeNode<Integer> node3 = new TreeNode<Integer>(4);
-        TreeNode<Integer> node4 = new TreeNode<Integer>(4);
+        TreeNode<Integer> node1 = new TreeNode<Integer>(2);
+        TreeNode<Integer> node2 = new TreeNode<Integer>(3);
+        TreeNode<Integer> node3 = new TreeNode<Integer>(5);
+        TreeNode<Integer> node4 = new TreeNode<Integer>(6);
 
         root.children.add(node1);
         root.children.add(node2);
