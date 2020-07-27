1acc5c2bba806e6f293e5e26682820be2d1dd283
shreya0167 <shreya.maheshwari06@gmail.com>
2020-07-28 01:27:00 +0530

    Postorder Binary Tree Traversal

---
+++ Binary Trees/postorderTraversalBinaryTree.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+class BinaryTreeNode<T> {
+    public BinaryTreeNode(T data){
+        this.data = data;
+    }
+    public T data;
+    public BinaryTreeNode<T> left;
+    public BinaryTreeNode<T> right;
+}
+
+class BinaryTreeUse {
+    public static void postorder(BinaryTreeNode<Integer> root){
+        if(root == null){
+            return;
+        }
+        postorder(root.left);
+        postorder(root.right);
+        System.out.print(root.data + " ");
+    }
+
+    public static void main(String[] args) {
+        BinaryTreeNode<Integer> root = new BinaryTreeNode<>(1);
+        BinaryTreeNode<Integer> node1 = new BinaryTreeNode<>(2);
+        root.left = node1;
+        BinaryTreeNode<Integer> node2 = new BinaryTreeNode<>(3);
+        root.right = node2;
+        BinaryTreeNode<Integer> node3 = new BinaryTreeNode<>(4);
+        BinaryTreeNode<Integer> node4 = new BinaryTreeNode<>(5);
+        node1.left = node3;
+        node1.right = node4;
+        BinaryTreeNode<Integer> node5 = new BinaryTreeNode<>(6);
+        BinaryTreeNode<Integer> node6 = new BinaryTreeNode<>(7);
+        node2.left = node5;
+        node2.right = node6;
+        postorder(root);
+    }
+}
+
+//Output: 4 5 2 6 7 3 1
