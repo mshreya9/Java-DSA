e1ebf3e3d3c02eeef0b78084087062fb630f644e
shreya0167 <shreya.maheshwari06@gmail.com>
2020-08-01 15:54:16 +0530

    Leaf-Similar Trees Leetcode Solution

---
+++ Leetcode/leaf-similarTrees.java
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+        List<Integer> list1 = new ArrayList<>();
+        List<Integer> list2 = new ArrayList<>();
+        leafTree(root1, list1);
+        leafTree(root2, list2);
+        
+        if(list1.equals(list2)){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    
+    public void leafTree(TreeNode root, List<Integer> list){
+        if(root == null){
+            return;
+        }
+        if(root.left == null && root.right == null){
+            list.add(root.val);
+        }
+        leafTree(root.left, list);
+        leafTree(root.right, list);
+    }
+}
