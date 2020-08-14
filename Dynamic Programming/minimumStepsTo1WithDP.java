8948dee5d1dcb9330bdb8c2403113f0d036d5c33
shreya0167 <shreya.maheshwari06@gmail.com>
2020-08-15 00:28:11 +0530

    Minimum Steps to 1 with DP

--- Dynamic Programming/minimumStepsTo1WithDP.java
+++ Dynamic Programming/minimumStepsTo1WithDP.java
@@ -1,29 +1,29 @@
 class MinimumSteps {
-    //Brute force - recursion
-    public static int countSteps(int n){
-       if(n == 1){
-           return 0;
-       }
-       int op1 = countSteps(n - 1);
-       int minSteps = op1;
-       if(n % 3 == 0){
-           int op2 = countSteps(n / 3);
-           if(op2 < minSteps){
-               minSteps = op2;
-           }
-       }
-       if(n % 2 == 0){
-           int op3 = countSteps(n / 2);
-           if(op3 < minSteps){
-               minSteps = op3;
-           }
-       }
-       return 1 + minSteps; 
+    public static int countStepsDP(int n){
+        int[] storage = new int[n+1];
+        storage[1] = 0;
+        for(int i = 2; i <= n; i++){
+            int min = storage[i - 1];
+            if(i % 3 == 0){
+                if(min > storage[i / 3]){
+                    min = storage[i / 3];
+                }
+            }
+            if(i % 2 == 0){
+                if(min > storage[i / 2]){
+                    min = storage[i / 2];
+                }
+            }
+            storage[i] = 1 + min; 
+        }
+        return storage[n];
     }
-
     
     public static void main(String[] args){
-        int n = 10;
-        System.out.println(countSteps(n));
+        int n = 100;
+        System.out.println(countStepsDP(n));
     }
 }
+
+//Output
+//7
