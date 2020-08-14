2ea68b34c4c2fca123afc95124e0abe451d28ca8
shreya0167 <shreya.maheshwari06@gmail.com>
2020-08-14 22:48:02 +0530

    Fibonacci with DP

---
+++ Dynamic Programming/fibonacciWithDP.java
@@ -0,0 +1,18 @@
+class Fibonacci {
+    public static int fibDP(int n){
+        int storage[] = new int[n+1];
+        storage[0] = 0;
+        storage[1] = 1;
+
+        for(int i = 2; i <= n; i++){
+            storage[i] = storage[i-1] + storage[i-2];
+        }
+
+        return storage[n];
+    }
+
+    public static void main(String[] args){
+        int n = 4;
+        System.out.println(fibDP(n));
+    }
+}
