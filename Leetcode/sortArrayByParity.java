e951d2a03f54281b6a51e6b71cb48068c1bfb3c2
shreya0167 <shreya.maheshwari06@gmail.com>
2020-08-21 21:38:46 +0530

    Sort array by parity in-place Leetcode Solution

--- Leetcode/sortArrayByParity.java
+++ Leetcode/sortArrayByParity.java
@@ -16,3 +16,44 @@ class Solution {
         return arr;
     }
 }
+
+//OR
+
+class Solution {
+    public int[] sortArrayByParity(int[] A) {
+        int[] arr = new int[A.length];
+        int k = 0;
+        int t = A.length - 1;
+        for(int i = 0; i < A.length; i++){
+            if(A[i] % 2 == 0){
+                arr[k++] = A[i];
+            }
+            else{
+                arr[t--] = A[i];
+            }
+        }
+        return arr;
+    }
+}
+
+//In-place
+class Solution {
+    public int[] sortArrayByParity(int[] A) {
+        int i = 0, j = A.length-1;
+        while(i < j){
+            if(A[i] % 2 == 0){
+                i++;
+            }
+            else{
+                int temp = A[i];
+                A[i] = A[j];
+                A[j] = temp;
+                if(A[i] % 2 == 0){
+                    i++;
+                }
+                j--;
+            }
+        }
+        return A;
+    }
+}
