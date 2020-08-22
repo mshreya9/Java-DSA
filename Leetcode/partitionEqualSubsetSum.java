8c191c43b0c8171dcc5b6c3c7ad478e44d4ca3fa
shreya0167 <shreya.maheshwari06@gmail.com>
2020-08-22 15:35:11 +0530

    Partition Equal Subset Sum Leetcode Solution

---
+++ Leetcode/partitionEqualSubsetSum.java
@@ -0,0 +1,40 @@
+class Solution {
+    public boolean canPartition(int[] nums) {
+        int n = nums.length;
+        int sum = 0;
+        for(int i = 0; i < n; i++){
+            sum = sum + nums[i];
+        }
+        if(sum % 2 != 0){
+            return false;
+        }
+        else{
+            return subsetSum(nums, sum/2, n);
+        }
+    }
+    public static boolean subsetSum(int arr[], int sum, int n){
+        boolean[][] t = new boolean[n+1][sum+1];
+        for(int i = 0; i < n+1; i++){
+            for(int j = 0; j < sum+1; j++){
+                if(i == 0){
+                    t[i][j] = false;
+                }
+                if(j == 0){
+                    t[i][j] = true;
+                }
+            }
+        }
+
+        for(int i = 1; i < n+1; i++){
+            for(int j = 1; j < sum+1; j++){
+                if(arr[i-1] <= j){
+                    t[i][j] = t[i-1][j-arr[i-1]] || t[i-1][j];
+                }
+                else{
+                    t[i][j] = t[i-1][j];
+                }
+            }
+        }
+        return t[n][sum];
+    }
+}
