4cd43c3974753aa93dd9cbcae34ab5446b71a6ca
shreya0167 <shreya.maheshwari06@gmail.com>
2020-08-29 16:53:33 +0530

    Last Occurrence Of An Element in Sorted Array

---
+++ Binary Search/lastOccurrenceOfAnElement.java
@@ -0,0 +1,30 @@
+class Solution {
+    public static int binarySearch(int[] arr, int elem){
+        int n = arr.length;
+        int start = 0;
+        int end = n-1;
+        int res = -1;
+
+        while(start <= end){
+            int mid = start + (end-start)/2;
+            if(elem == arr[mid]){
+                res = mid;
+                start = mid + 1;
+            }
+            else if(elem < arr[mid]){
+                end = mid - 1;
+            }
+            else{
+                start = mid + 1;
+            }
+        }
+        return res;
+    }
+    public static void main(String[] args) {
+        int[] arr = {2, 4, 10, 10, 10, 18, 20};
+        int elem = 10;
+        System.out.println(binarySearch(arr, elem));
+    }
+}
+
+//Output: 4
