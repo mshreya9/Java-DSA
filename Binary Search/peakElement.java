32fcfc26409600885d78df206c02fc855f07afc1
shreya0167 <shreya.maheshwari06@gmail.com>
2020-08-31 22:56:50 +0530

    Peak Element

---
+++ Binary Search/peakElement.java
@@ -0,0 +1,46 @@
+class Solution {
+    public static int binarySearch(int[] arr){
+        int n = arr.length;
+        int start = 0;
+        int end = n-1;
+
+        while(start <= end){
+            int mid = start + (end-start)/2;
+            if(mid > 0 && mid < n-1){
+                if(arr[mid] > arr[mid+1] && arr[mid] > arr[mid-1]){
+                    return mid;
+                }
+                else if(arr[mid-1] > arr[mid]){
+                    end = mid-1;
+                }
+                else{
+                    start = mid+1;
+                }
+            }
+            else if(mid == 0){
+                if(arr[0] > arr[1]){
+                    return 0;
+                }
+                else{
+                    return 1;
+                }
+            }
+            else if(mid == n-1){
+                if(arr[n-1] > arr[n-2]){
+                    return n-1;
+                }
+                else{
+                    return n-2;
+                }
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int[] arr = {5,10,20,15};
+        System.out.println(binarySearch(arr));
+    }
+}
+
+//Print index of the peak element
+//Output: 2
