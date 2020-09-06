dacc926b89dbe4aecf9e60e820986fc6f7a07b8e
shreya0167 <shreya.maheshwari06@gmail.com>
2020-09-06 14:26:58 +0530

    Number of Students Doing Homework at a Given Time

---
+++ Leetcode/numberOfStudentsDoingHomeworkAtGivenTime.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int busyStudent(int[] startTime, int[] endTime, int queryTime) {
+        int count = 0;
+        for(int i = 0; i < endTime.length; i++){
+            if(queryTime <= endTime[i] && queryTime >= startTime[i]){
+                count++;
+            }
+        }
+        return count;
+    }
+}
