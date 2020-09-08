25eadcce40c7d6e2855aec8633ccc9ad892571aa
shreya0167 <shreya.maheshwari06@gmail.com>
2020-09-08 21:29:31 +0530

    Binary Tree Level Order Traversal II Leetcode

---
+++ Leetcode/binaryTreeLevelOrderTraversal2.java
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> list = new ArrayList<>();
+        
+        if(root == null){
+            return list;
+        }
+        
+        LinkedList<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        int length = 0;
+        while(!queue.isEmpty()) {
+            length = queue.size();
+            List<Integer> row = new ArrayList<>();
+            while(length > 0) {
+                TreeNode rm = queue.remove();
+                row.add(rm.val);
+                if(rm.left  != null){
+                    queue.add(rm.left);
+                }
+                if(rm.right != null){
+                    queue.add(rm.right);
+                }
+                length--;
+            }
+            list.add(row);
+        }
+        Collections.reverse(list);
+        return list;
+    }
+}
