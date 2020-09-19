c1dd3f9291bec617a541e11294ea9b9cd6502569
shreya0167 <shreya.maheshwari06@gmail.com>
2020-09-19 23:59:00 +0530

    Check if a word occurs as a prefix of any word in a sentence

---
+++ Leetcode/checkIfAWordOccursAsAPrefixOfAnyWordInASentence.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int isPrefixOfWord(String sentence, String searchWord) {
+        String[] arr = sentence.split(" ");
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i].length() >= searchWord.length()){
+                if(searchWord.equals(arr[i].substring(0, searchWord.length()))){
+                    return i+1;
+                }
+            }
+        }
+        return -1;
+    }
+}
