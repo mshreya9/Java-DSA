b9b42bec666ba5953a9a7b7d977bd6e4e9391d1b
shreya0167 <shreya.maheshwari06@gmail.com>
2020-09-22 22:41:47 +0530

    Majority Element II Leetcode

--- Leetcode/majorityElementII.java
+++ Leetcode/majorityElementII.java
@@ -20,3 +20,56 @@ class Solution {
         return list;
     }
 }
+
+//Boyer Moore Voting Algorithm
+//There can be maximum 2 majority elements and minimum zero
+//Time Complexity : O(N)
+//Space Complexity : O(1)
+
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+        int n = nums.length;
+        int num1 = -1, num2 = -1, count1 = 0, count2 = 0;
+        for(int i = 0; i < n; i++){
+            if(nums[i] == num1){
+                count1++;
+            }
+            else if(nums[i] == num2){
+                count2++;
+            }
+            else if(count1 == 0){
+                num1 = nums[i];
+                count1 = 1;
+            }
+            else if(count2 == 0){
+                num2 = nums[i];
+                count2 = 1;
+            }
+            else{
+                count1--;
+                count2--;
+            }
+        }
+        
+        List<Integer> list = new ArrayList<>();
+        count1 = count2 = 0;
+        for(int i = 0; i < n; i++){
+            if(nums[i] == num1){
+                count1++;
+            }
+            if(nums[i] == num2){
+                count2++;
+            }
+        }
+        
+        if(count1 > n/3){
+            list.add(num1);
+        }
+        if(count2 > n/3){
+            list.add(num2);
+        }
+        
+        return list;
+    }
+}
+
