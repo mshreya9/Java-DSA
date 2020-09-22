65836cca18e364008a4be6f0897ead5e083c71ba
shreya0167 <shreya.maheshwari06@gmail.com>
2020-09-22 18:11:23 +0530

    Majority Element Leetcode

--- Leetcode/majorityElement.java
+++ Leetcode/majorityElement.java
@@ -20,3 +20,24 @@ class Solution {
         return -1;
     }
 }
+
+//Moore Voting Algorithm
+
+class Solution {
+    public int majorityElement(int[] nums) {
+        int count = 0;
+        int candidate = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(count == 0){
+                candidate = nums[i];
+            }
+            if(nums[i] == candidate){
+                count += 1;
+            }
+            else{
+                count -= 1;
+            }
+        }
+        return candidate;
+    }
+}
