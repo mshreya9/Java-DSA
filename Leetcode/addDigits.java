297d9a7e9021700ff620d695711cd32331b158a2
shreya0167 <shreya.maheshwari06@gmail.com>
2020-10-29 23:57:55 +0530

    Add Digits Leetcode

--- Leetcode/addDigits.java
+++ Leetcode/addDigits.java
@@ -9,3 +9,18 @@ class Solution {
         return(addDigits(x + num));
     }
 }
+//Another solution - really interesting trick:
+
+class Solution {
+    public int addDigits(int num) {
+        if(num == 0){
+            return 0;
+        }
+        else if(num % 9 == 0){
+            return 9;
+        }
+        else{
+            return num % 9;
+        }
+    }
+}
